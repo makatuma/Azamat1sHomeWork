f36545b89b91eb22922be97c77a408ad8af78005
makatuma <dkc.dov@gmail.com>
2020-07-01 18:45:43 -0500

    asvsdvafdb

--- src/test/java/API/Pojo/StarWars/ResultsPojo.java
+++ src/test/java/API/Pojo/StarWars/ResultsPojo.java
@@ -9,7 +9,7 @@ public class ResultsPojo {
 
     private String name;
     private String model;
-    private String manufactur
+    private String manufacturer;
     private String cost_in_credits;
     private String length;
     private String max_atmosphering_speed;
